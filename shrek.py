3be41614b0bd5f13272804b2b9de1517d3d84274
Hamza Krika <63921551+Hemzyy@users.noreply.github.com>
2020-05-17 18:53:51 +0200

    Update shrek.py

--- shrek.py
+++ shrek.py
@@ -4,7 +4,7 @@ import urllib.request
 
 keyboard = Controller()
 
-f = urllib.request.urlopen("https://gist.githubusercontent.com/Hemzyy/581b034b7330bb2150a636e8c7eae370/raw/90aa78564df9760490da85d2a310f95c7ddf4554/shrekscript.txt")
+f = urllib.request.urlopen("https://gist.githubusercontent.com/Hemzyy/581b034b7330bb2150a636e8c7eae370/raw/f482a9aaebb994078000e12574b79a4aa006f8c8/shrekscript.txt")
 script = f.read()
 
 scriptLines = script.decode().split('\n\n')
